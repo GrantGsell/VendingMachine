0ccffcb7aaf99210b4a480f3a92c208a10ff9f3d
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-11 15:46:11 -0400

    Initial maven project creation with packages

---
+++ VendingMachine/src/main/java/vendingmachine/VendingMachine.java
@@ -0,0 +1,12 @@
+package vendingmachine;
+
+/**
+ *
+ * @author Grant
+ */
+public class VendingMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
