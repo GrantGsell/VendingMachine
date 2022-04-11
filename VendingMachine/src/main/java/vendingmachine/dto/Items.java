dafc01b0097ff07612b9d5e119e18791789ab77b
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-11 16:09:55 -0400

    Added fields, getters, setters

--- VendingMachine/src/main/java/vendingmachine/dto/Items.java
+++ VendingMachine/src/main/java/vendingmachine/dto/Items.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vendingmachine.dto;
+
+import java.math.BigDecimal;
 
 /**
  *
  * @author Grant
  */
 public class Items {
+    // Item fields
+    private String name;
+    private BigDecimal price;
+    private int stock;
+    private String selectionCode;
+
+    // Class getters
+    public String getName() {
+        return name;
+    }
     
+    public BigDecimal getPrice() {
+        return price;
+    }
+    
+    public int getStock() {
+        return stock;
+    }
+
+    public String getSelectionCode(){
+        return selectionCode;
+    }
+    
+    // Class setters
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    public void setSelectionCode(String code){
+        this.selectionCode = code;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 }
