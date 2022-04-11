9be3523f2db9349da42cba4df2a0c45ed16bc6ad
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-11 17:13:40 -0400

    Added hashcode, equals, to string method overrides

--- VendingMachine/src/main/java/vendingmachine/dto/Items.java
+++ VendingMachine/src/main/java/vendingmachine/dto/Items.java
@@ -1,6 +1,7 @@
 package vendingmachine.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  *
@@ -46,4 +47,46 @@ public class Items {
     public void setName(String name) {
         this.name = name;
     }
+    
+    
+    // HashCode, Equals, To String overrides
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.price);
+        hash = 67 * hash + this.stock;
+        hash = 67 * hash + Objects.hashCode(this.selectionCode);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Items other = (Items) obj;
+        if (this.stock != other.stock) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.selectionCode, other.selectionCode)) {
+            return false;
+        }
+        return Objects.equals(this.price, other.price);
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" + "name=" + name + ", price=" + price + ", stock=" + stock + ", selectionCode=" + selectionCode + '}';
+    }
+    
 }
