76ff2bf8d8929472afc55917024ba54da0fd4dfa
jcbrito <jcbritov2@gmail.com>
2022-04-12 10:18:36 -0400

    first commit

--- VendingMachine/src/main/java/vendingmachine/service/VendingMachineServiceLayer.java
+++ VendingMachine/src/main/java/vendingmachine/service/VendingMachineServiceLayer.java
@@ -4,10 +4,25 @@
  */
 package vendingmachine.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import vendingmachine.dto.Items;
+
 /**
  *
  * @author Grant
  */
 public interface VendingMachineServiceLayer {
+    
+    public Items buyItem(Items item);
+    
+    public BigDecimal getChange();
+    
+    public List<Items> purchaseable();
+    
+    public void addCredit(BigDecimal amount);
+    
+    public BigDecimal checkCredit();
+    
     
 }
